273fef721df54af1701e4c733359d9cd1661351a
AWERDdev <kokorami565@gmail.com>
2025-01-29 21:57:34 +0200

    frontend completed

---
+++ client/src/App.jsx
@@ -0,0 +1,58 @@
+import './App.css'
+import {useState} from 'react'
+import NavBar from './components/NavBar'
+
+function App() {
+  const [URL,setURL] = useState('')
+  
+  const sendData = ()=>{
+    console.log(URL)
+  }
+
+  return (
+    <>
+<main className='h-screen bg-gradient-to-b from-blue-100 to-white min-w-[650px]  '>
+  <div className="Nav-Bar">
+    <NavBar/>
+  </div>
+    <div className="Title-Container min-w-[650px]">
+
+      <div className="Title  grid justify-center items-center">
+        <h1 className='text-[2.5rem] text-blue-600 font-bold'>Web Scraper</h1>
+      </div>
+
+      <div className="context-Container  grid justify-center items-center ">
+        <p className='context text-gray-600 text-[1.5rem]'>Extract data from any website with ease</p>
+      </div>
+
+    </div>
+
+  <div className="Scraper-container grid justify-center items-center w-auto h-auto">
+    <div className="container  bg-white h-auto w-[55vw]  box-shadow-lg rounded-md pr-5 pl-5 pb-5 ">
+    
+      <div className="Context">
+        <h1 className='text-[1.5rem] p-1'>Start Scraping</h1>
+        <p className='text-gray-600 p-1'>Enter a URL to begin extracting data</p>
+      </div>
+    
+      <div className="URLinput-container  mt-[1rem] p-1">
+        <input type="text"
+         className="URL w-full h-[5vh] rounded-md p-2 font-thin text-[0.9rem] "
+         required
+          placeholder='https://example.com'
+           onChange={(e)=>{setURL(e.target.value)}}
+           />
+      </div>
+      <div className="button-container mt-[1rem] flex justify-center p-1">
+        <button className="Scrape-BTN w-full bg-[#0D1117] rounded-md text-white h-[5vh] hover:bg-[hsl(216,28%,15%)] "onClick={sendData}>Scrape</button>
+      </div>
+    </div>
+  </div>
+
+</main>
+    </>
+  )
+}
+
+export default App
+;
